22b89fe2e39ad7d5ffa2db6ebf9b09d161ac438f
Kevin Martensson <kevinmartensson@gmail.com>
2015-11-06 11:04:03 +0100

    ES2015ify tests

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
-'use strict';
-var assert = require('assert');
-var isPathInside = require('./');
+import test from 'ava';
+import fn from './';
 
-it('should check if a path is inside another path', function () {
-	assert(isPathInside('a/b/c', 'a/b'));
-	assert(isPathInside('/a/b/c', '/a/b'));
-	assert(!isPathInside('a/b', 'a/b'));
-	assert(!isPathInside('/a/b', '/a/b'));
+test('should check if a path is inside another path', t => {
+	t.true(fn('a/b/c', 'a/b'));
+	t.true(fn('/a/b/c', '/a/b'));
+	t.false(fn('a/b', 'a/b'));
+	t.false(fn('/a/b', '/a/b'));
+	t.end();
 });
