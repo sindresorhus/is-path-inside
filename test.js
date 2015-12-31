732f460b7479ad5b82e88c8a9969ce84c188a56a
Sindre Sorhus <sindresorhus@gmail.com>
2016-01-01 00:50:34 +0100

    update tests for latest AVA version

--- test.js
+++ test.js
@@ -1,10 +1,9 @@
 import test from 'ava';
 import fn from './';
 
-test('should check if a path is inside another path', t => {
+test(t => {
 	t.true(fn('a/b/c', 'a/b'));
 	t.true(fn('/a/b/c', '/a/b'));
 	t.false(fn('a/b', 'a/b'));
 	t.false(fn('/a/b', '/a/b'));
-	t.end();
 });
